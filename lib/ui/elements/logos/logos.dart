fa92be48e93597dd2e69f0fc7df6c9e4b1e0fc7f
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2024-12-12 15:19:56 -0300

    fix: const  tractian logo

--- lib/ui/elements/logos/logos.dart
+++ lib/ui/elements/logos/logos.dart
@@ -10,7 +10,7 @@ class UiLogos extends StatelessWidget {
   final double? height;
   final double? width;
 
-  const UiLogos({
+  const UiLogos.tractian({
     super.key,
     this.color = UiColorsConstant.white50,
     this.height,
