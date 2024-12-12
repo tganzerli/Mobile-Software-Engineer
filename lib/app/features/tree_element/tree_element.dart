2dda4ffddc67c7ac5e1e3e311437cae76b1f1e6f
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2024-12-12 07:59:29 -0300

    cry: implement the infector in tree element feat.

--- lib/app/features/tree_element/tree_element.dart
+++ lib/app/features/tree_element/tree_element.dart
@@ -1,6 +1,16 @@
 import 'package:auto_injector/auto_injector.dart';
 
+import 'domain/repositories/tree_element_repository.dart';
+import 'domain/usecases/get_elements_usecase.dart';
+import 'external/datasources/tree_element_datasource_impl.dart';
+import 'infra/datasources/tree_element_datasource.dart';
+import 'infra/repositories/tree_element_repository_impl.dart';
+
 final treeElementFeature = AutoInjector(
   tag: 'treeElementFeature',
-  on: (i) {},
+  on: (i) {
+    i.add<TreeElementDatasource>(TreeElementDatasourceImpl.new);
+    i.add<TreeElementRepository>(TreeElementRepositoryImpl.new);
+    i.add<GetTreeElementsUsecase>(GetTreeElementsUsecaseImpl.new);
+  },
 );
